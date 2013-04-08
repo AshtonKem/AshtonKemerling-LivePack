606cd4b75dae334a7034696b7411bd1afe67f656
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-04-08 13:39:41 -0500

    Remove magit, turns out emacs-live provides it.

--- init.el
+++ init.el
@@ -6,8 +6,7 @@
 ;; Load bindings config
 (setq test-sym "abc")
 (live-load-config-file "bindings.el")
-(add-to-list 'load-path "lib/epy")
+(add-to-list 'load-path (concat default-directory "lib/epy"))
 (add-to-list 'load-path "lib/epy/extensions")
-(require 'magit)
 (require 'epy-nose)
 (require 'virtualenv)
