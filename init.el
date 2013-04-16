69b9ed538b8a5c1fe74cfe54206616c88ee02aa0
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-04-16 14:05:24 -0500

    Pylint!!

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 (live-add-pack-lib "epy")
+(live-add-pack-lib "pylint")
 (live-add-pack-lib "epy/extensions")
 (require 'epy-nose)
 (require 'virtualenv)
@@ -14,3 +15,7 @@
 ; This assumes that you've installed aspell via brew.
 (setq ispell-program-name "aspell")
 (add-to-list 'exec-path "/usr/local/bin")
+
+; Pylint
+(autoload 'python-pylint "python-pylint")
+(autoload 'pylint "python-pylint")
