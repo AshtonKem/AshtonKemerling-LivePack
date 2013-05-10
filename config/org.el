13838a8361358df9216296fbe4c13a45eddc378e
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-05-09 22:12:25 -0500

    Require & configure org-jira.

--- config/org.el
+++ config/org.el
@@ -2,3 +2,6 @@
                          "~/org/projects.org"))
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+(setq jiralib-url "https://jira.n-s.us/")
+(require 'org-jira)
