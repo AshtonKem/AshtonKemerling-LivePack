ced4fa996751de1fe7ff0bdbc84147686b1a0c3c
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-05-18 11:54:17 -0500

    New butler arguments

--- config/butler.el
+++ config/butler.el
@@ -1,6 +1,6 @@
 (add-to-list 'load-path "/Users/ashtonkemerling/Documents/Butler")
 (require 'butler)
-(add-to-list 'butler-servers
+(add-to-list 'butler-server-list
              '(jenkins "Jenkins"
                        (server-address . "https://jenkins.n-s.us/jenkins/view/Data%20Engine/")
                        (auth-file . "~/.authinfo.gpg")))
