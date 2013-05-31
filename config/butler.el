3d3069ee850f113a61c8b75b6dc60bee66311ac5
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-05-31 10:27:30 -0500

    Watch NS main Jenkins.

--- config/butler.el
+++ config/butler.el
@@ -2,5 +2,5 @@
 (require 'butler)
 (add-to-list 'butler-server-list
              '(jenkins "Jenkins"
-                       (server-address . "https://jenkins.n-s.us/jenkins/view/Data%20Engine/")
+                       (server-address . "https://jenkins.n-s.us/jenkins/")
                        (auth-file . "~/.authinfo.gpg")))
