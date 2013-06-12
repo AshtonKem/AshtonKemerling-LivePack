62c5b10148c38abda9b1b69c955f7f51ad257cd2
Ashton Kemerling <ashtonkemerling@gmail.com>
2013-06-12 16:01:42 -0500

    Clojure indentation.

--- init.el
+++ init.el
@@ -40,3 +40,17 @@
 (setq-default bm-buffer-persistence t)
 (require 'jabber)
 (add-hook 'jabber-chat-mode-hook 'goto-address)
+
+
+;; Clojure
+(require 'clojure-mode)
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
