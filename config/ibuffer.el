966c8429946a28ea1e38a4ae968781da4949f477
Ashton Kemerling <ashtonkemerling@Ashtons-MacBook-Pro.local>
2013-07-21 00:19:05 -0500

    Neat ibuffer filtering commands.

---
+++ config/ibuffer.el
@@ -0,0 +1,33 @@
+(require 'ibuffer)
+(setq ibuffer-saved-filter-groups
+  (quote (("default"
+            ("Org" ;; all org-related buffers
+              (mode . org-mode))
+            ("Mail"
+              (or  ;; mail-related buffers
+               (mode . message-mode)
+               (mode . mail-mode)
+               ;; etc.; all your mail related modes
+               ))
+            ("Magit"
+             (mode . magit-mode))
+            ("Care For Real"
+              (filename . "Documents/cfr-app"))
+            ("Work"
+             (or
+              (filename . "Documents/ns_data")
+              (filename . "Documents/NS_text_tools")))
+            ("Scheduling"
+             (filename . "Documents/scheduler"))
+            ("Programming" ;; prog stuff not already in MyProjectX
+              (or
+                (mode . c-mode)
+                (mode . perl-mode)
+                (mode . python-mode)
+                (mode . emacs-lisp-mode)
+                ;; etc
+                ))))))
+
+(add-hook 'ibuffer-mode-hook
+  (lambda ()
+    (ibuffer-switch-to-saved-filter-groups "default")))
