16c7a3b404d142e21017e51a6ee178e807f8de5b
Ashton Kemerling <akemerling@narrativescience.com>
2013-10-01 21:45:39 -0600

    Agendas.

--- config/org.el
+++ config/org.el
@@ -3,6 +3,11 @@
                          "~/org/projects.org"
                          "~/org/chores.org"
                          "~/org/wedding.org"))
+
+(setq org-agenda-custom-commands
+           '(("h" tags "home")
+             ("g" tags "government")
+             ("s" tags "shopping")))
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 
