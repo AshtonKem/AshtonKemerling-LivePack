050d265826590585925d46fbe3b581ddafa55dea
Ashton Kemerling <akemerling@narrativescience.com>
2013-10-02 09:07:58 -0600

    Actually, I don't need org-jira at all.

--- config/org.el
+++ config/org.el
@@ -7,6 +7,6 @@
       '(("h" tags "home+TODO=\"NEXT\"")
         ("g" tags "government+TODO=\"NEXT\"")
         ("s" tags "shopping+TODO=\"NEXT\"")))
+
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(require 'org-jira)
