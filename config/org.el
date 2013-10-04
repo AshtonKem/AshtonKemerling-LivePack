bc7cca4f5ad5389a1aaa976aea49c4ffdc57732e
Ashton Kemerling <akemerling@narrativescience.com>
2013-10-04 11:13:54 -0600

    Agendas should contain in-progress items too.

--- config/org.el
+++ config/org.el
@@ -4,9 +4,9 @@
                          "~/org/wedding.org"))
 
 (setq org-agenda-custom-commands
-      '(("h" tags "home+TODO=\"NEXT\"")
-        ("g" tags "government+TODO=\"NEXT\"")
-        ("s" tags "shopping+TODO=\"NEXT\"")))
+      '(("h" tags "home+TODO=\"NEXT\"|home+TODO=\"IN-PROGRESS\"")
+        ("g" tags "government+TODO=\"NEXT\"|government+TODO=\"IN-PROGRESS\"")
+        ("s" tags "shopping+TODO=\"NEXT\"|shopping+TODO=\"IN-PROGRESS\"")))
 
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
