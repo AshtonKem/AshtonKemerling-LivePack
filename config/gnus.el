b798e26c89f6e0012105f47acfb59d2997a45033
Ashton Kemerling <akemerling@narrativescience.com>
2013-10-19 19:23:02 -0600

    Gnus for multiple accounts again.

--- config/gnus.el
+++ config/gnus.el
@@ -1,44 +1,22 @@
 (require 'gnus)
-
-(setq gnus-select-method
-      '(nnimap "home"
-               (nnimap-address "imap.gmail.com")
-               (nnimap-server-port 993)
-               (nnimap-stream ssl)
-               (nnimap-list-pattern
-                ("INBOX" "*"))
-               (nnimap-authinfo-file "~/.authinfo.gpg")))
-
-(setq gnus-parameters
-      '(("nnimap+home:*"
-         (display . all)
-         (posting-style
-          (name "Ashton Kemerling")
-          (address "ashtonkemerling@gmail.com")
-          (signature-file "~/.signature"))
-         (expiry-target . delete))
-        ("nnimap+home:[Gmail]/.*"
-          (display . all)
-          (posting-style
-           (name "Ashton Kemerling")
-           (address "ashtonkemerling@gmail.com")
-           (signature-file "~/.signature"))
-          (expiry-wait . never))))
+(setq gnus-select-method '(nnnil))
+(setq gnus-secondary-select-methods
+      '((nnimap "home"
+                (nnimap-address "imap.gmail.com")
+                (nnimap-server-port 993)
+                (nnimap-stream ssl)
+                (nnimap-authinfo-file "~/.authinfo.gpg"))
+        (nnimap "work"
+                (nnimap-address "imap.gmail.com")
+                (nnimap-server-port 993)
+                (nnimap-stream ssl)
+                (nnimap-authinfo-file "~/.authinfo.gpg"))))
 
 (setq gnus-thread-sort-functions
       '((not gnus-thread-sort-by-date)
         (not gnus-thread-sort-by-number)))
 
 (setq gnus-permanently-visible-groups "INBOX")
-
-(setq gnus-posting-styles
-             '((".*"
-                (address "ashtonkemerling@gmail.com")
-                (name "Ashton Kemerling"))
-               (".*home:.*"
-                (address "ashtonkemerling@gmail.com")
-                (name "Ashton Kemerling"))))
-
 (setq message-send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
       smtpmail-auth-credentials '(("smtp.gmail.com" 587 "ashtonkemerling@gmail.com" nil))
