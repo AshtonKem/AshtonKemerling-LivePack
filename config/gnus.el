61b305be7905a19b1ae7d4014e8daa007a8d1680
Ashton Kemerling <akemerling@narrativescience.com>
2013-10-26 14:38:51 -0600

    Add gwene as the primary select method.

--- config/gnus.el
+++ config/gnus.el
@@ -1,5 +1,5 @@
 (require 'gnus)
-(setq gnus-select-method '(nnnil))
+(setq gnus-select-method '(nntp "news.gwene.org"))
 (setq gnus-secondary-select-methods
       '((nnimap "home"
                 (nnimap-address "imap.gmail.com")
