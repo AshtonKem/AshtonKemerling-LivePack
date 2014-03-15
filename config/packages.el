5c39259405be6f1d3c1057ef22532fbf47b9685b
Ashton Kemerling <ashtonkemerling@gmail.com>
2014-03-14 23:24:39 -0600

    Don't load packages unless you need to, it's slow.

--- config/packages.el
+++ config/packages.el
@@ -3,11 +3,19 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
-(package-refresh-contents)
+
+(defvar packages-loaded nil)
+(defun load-packages ()
+  (unless packages-loaded
+    (package-refresh-contents)
+    (setq packages-loaded t))
+  nil)
+
 
 (mapc
  (lambda (package)
    (or (package-installed-p package)
+       (load-packages)
        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
            (package-install package))))
  '(powerline diminish helm helm-ls-git
