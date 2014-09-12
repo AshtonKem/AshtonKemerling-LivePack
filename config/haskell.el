ee872c64eaca726e25f90d8fde43c5fd16e9aca8
Ashton Kemerling <ashtonkemerling@gmail.com>
2014-09-11 19:49:21 -0600

    Sorry Haskell mode.

--- config/haskell.el
+++
@@ -1,8 +0,0 @@
-(require 'haskell-mode)
-(require 'flycheck-haskell)
-
-(add-hook 'haskell-mode-hook (lambda ()
-                               (haskell-indentation-mode 1) ;; turn off, just to be sure
-                               (haskell-indent-mode -1)       ;; turn on indent-mode
-                               (flycheck-mode 1)
-                               (setq locale-coding-system 'utf-8)))
